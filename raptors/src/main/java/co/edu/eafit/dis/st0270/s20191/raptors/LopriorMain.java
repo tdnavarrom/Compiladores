4d294dc1434479b1fde63f1f3b0c9b2cc04561f1
tdnavarrom <tdnavarrom@7faf04c5-c4d1-4ece-8bb5-b756ae20663d>
2019-06-06 11:40:32 +0000

    ya que

---
+++ raptors/src/main/java/co/edu/eafit/dis/st0270/s20191/raptors/LopriorMain.java
@@ -0,0 +1,69 @@
+package co.edu.eafit.dis.st0270.s20191.raptors;
+
+
+import co.edu.eafit.dis.st0270.s20191.raptors.parser.LopriorParser;
+import co.edu.eafit.dis.st0270.s20191.raptors.flex.LopriorLexer;
+
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class LopriorMain {
+	public static boolean s = false;
+	public static boolean p = false;
+	public static boolean o = false;
+	
+    public static void main( String[] args )
+    {
+		
+		for(int i=0;i<args.length;i++){
+			System.out.print("En: "+i+" : "+args[i]+"|");
+		}
+		System.out.println();
+		int columna=0;
+		String comand = args[columna];
+		if (comand.equals("-s")){
+			columna++;
+			s=true;
+			System.out.println("S Selected");
+		} else if(comand.equals("-o")){
+			columna++;
+			o=true;
+			System.out.println("O Selected");
+		} else if(comand.equals("-p")){
+			columna++;
+			p=true;
+			System.out.println("P Selected");
+		}
+		
+        boolean verificador = false;
+        for(int i = columna; i < args.length; i++){
+            try {	
+                FileReader fr = new FileReader(args[columna]);
+                LopriorLexer lexer = new LopriorLexer(fr);
+                LopriorParser parser = new LopriorParser(lexer);
+                verificador = true;
+                parser.parse();
+            } catch (FileNotFoundException info ) {
+                System.err.println("File: " + args[i] + " not found");
+            } catch (IOException ioe){
+                System.err.println("IOException: " + args[i]);
+            } catch (Exception e){
+                verificador = false;
+                System.err.println("File: " + args[i] + " Parser: " + false);
+            }
+
+            if(verificador == true && !s && !o) System.out.println("File: " + args[i] + " Parser: " + true);
+
+        }       
+    }
+	
+	
+	public static void usage() {
+		System.err.println("Uso: java TeddyMain [-s] [-p] [-n] [-f] <ficheros> ...");
+		System.exit(1);
+    }
+    
+    
+}
